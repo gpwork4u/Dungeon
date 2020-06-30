3d90ea86849cc2831e9bd8ed72038a22cfd218fd
GP <gpwork4u@gmail.com>
2020-06-30 09:54:59 +0800

    Change dugeon data type to array

--- infinite_dugeon.py
+++ infinite_dugeon.py
@@ -1,5 +1,6 @@
 import os
 import random
+import numpy as np
 import monsters
 class Direction:
     LEFT = 0
@@ -14,29 +15,27 @@ class Wall_type:
         return random.randint(0, 1)
 
 class Block:
-    def __init__(self, block=None, from_=None):
+    def __init__(self):
+        self.builded = False
+
+    def build(self, from_=None, walls=[]):
+        self.builded = True
         self.boss_room = not bool(random.randint(0,100))
         if self.boss_room:
             self.objects = [monsters.Slime(hp=100)]
         else:
             self.objects = [monsters.Slime()]
-        self._direction = [Wall_type.random() for i in range(4)]
-        if block:
-            self._direction[from_-2] = block
-    
-    def move_to(self, direction):
-        if self._direction[direction]:
-            if self._direction[direction] == Wall_type.DOOR:
-                self._direction[direction] = Block(block=self, from_=direction)
-            return self._direction[direction]
-        return self
-
+        self.direction = [Wall_type.random() for i in range(4)]
+        if from_ != None:
+            self.direction[from_-2] = Wall_type.DOOR
+        for wall in walls:
+            self.direction[wall] = Wall_type.WALL
     def add_object(self, object_):
         self.objects.append(object_)
 
     def __str__(self):
         walls = ['|', '--', '|', '--']
-        for i, wall in enumerate(self._direction):
+        for i, wall in enumerate(self.direction):
             if wall:
                 if i % 2:
                     walls[i] = '  '
@@ -71,10 +70,17 @@ class Player:
         self.attack(target)
 
 if __name__ == '__main__':
-    current_block = Block()
     player = Player()
     move_actions = ['left', 'front', 'right', 'back', 'attack']
+    dugeon = [[Block() for j in range(3)] for i in range(3)]
+    dugeon = np.array(dugeon)
+    coor_x = 1
+    coor_y = 1
+    current_coor = (coor_x, coor_y)
+    current_block = dugeon[current_coor]
+    current_block.build()
     while True:
+        print(current_coor)
         print(current_block)
         for i, act in enumerate(move_actions):
             print('<%d>%s,'%(i, act),end='')
@@ -82,7 +88,32 @@ if __name__ == '__main__':
         print('enter action:')
         action = int(input())
         if action < 4:
-            current_block = current_block.move_to(action)
+            if not current_block.direction[action]:
+                continue
+            x_move = 0
+            y_move = 0
+            direction = 1
+            if action%2 == 0:
+                x_move = 1
+            else:
+                y_move = -1
+            if action < 2:
+                direction = -1
+            coor_x += direction*x_move
+            coor_y += direction*y_move
+            current_coor = (coor_x, coor_y)
+            current_block = dugeon[current_coor]
+            if not current_block.builded:
+                walls = []
+                if coor_x == dugeon.shape[0]-1:
+                    walls.append(Direction.RIGHT)
+                if coor_x == 0:
+                    walls.append(Direction.LEFT)
+                if coor_y == dugeon.shape[1]-1:
+                    walls.append(Direction.FRONT)
+                if coor_y == 0:
+                    walls.append(Direction.BACK)
+                current_block.build(from_=action, walls=walls)
         else:
             print('<%d>%s,'%(0, 'cancel'),end='')
             for i,target in enumerate(current_block.objects):
